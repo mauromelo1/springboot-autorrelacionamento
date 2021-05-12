bb8e19985e2b390e0fb01c991cccba5154f2fd36
Mauro Melo <mauromelo1@gmail.com>
2021-05-12 15:15:18 -0300

    Projeto criado

---
+++ src/main/java/com/mauromelo1/autorrelacionamento/AutorrelacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.mauromelo1.autorrelacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorrelacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutorrelacionamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mauromelo1/autorrelacionamento/AutorrelacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauromelo1.autorrelacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorrelacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
