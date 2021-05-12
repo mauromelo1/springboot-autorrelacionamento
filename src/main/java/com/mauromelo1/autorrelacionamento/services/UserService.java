de4334c8bdf3ba32fffbc5a404949e0bb8208f79
Mauro Melo <mauromelo1@gmail.com>
2021-05-12 15:34:51 -0300

    UserDTO and UserService

---
+++ src/main/java/com/mauromelo1/autorrelacionamento/services/UserService.java
@@ -0,0 +1,25 @@
+package com.mauromelo1.autorrelacionamento.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mauromelo1.autorrelacionamento.dto.UserDTO;
+import com.mauromelo1.autorrelacionamento.entities.User;
+import com.mauromelo1.autorrelacionamento.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<UserDTO> findAll() {
+		List<User> result = repository.findAll();
+		return result.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	}
+}
